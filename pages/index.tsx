edbcaf9958289fcc9dccd4d7bba15ae5b01fa924
Bartek <wonderfulLifeVision@gmail.com>
2022-12-08 20:49:13 +0100

    feat: use <ModalComponent + use endpoint for it + add useState 'pickedCategory' for it + function handler for it

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,24 +15,30 @@ const fetcher = (url: string) => fetch(url).then((res) => res.json())
 export default function Index() {
   const [searchValue, setSearchValue] = useState('')
   const [page, setPage] = useState(1)
+  const [pickedCategory, setPickedCategory] = useState(0)
 
   const resultsPerPage = 6
 
   //                              /api/posts/category/startPagination/endPagination/title
-  const { data, error } = useSWR(`/api/posts/0/${(page - 1) * resultsPerPage}/${page * resultsPerPage}/${searchValue}`, fetcher)
+  const { data, error } = useSWR(`/api/posts/${pickedCategory}/${(page - 1) * resultsPerPage}/${page * resultsPerPage}/${searchValue}`, fetcher)
+  const { data: categories, error: errorCategories } = useSWR("/api/categories", fetcher)
 
   const handleSearch = (text: string) => {
     setSearchValue(text)
   }
 
-  if (error) return <div>Failed to load</div>
+  const handleCategoryClick = (id: number) => {
+    setPickedCategory(id)
+  }
+
+  if (error | errorCategories) return <div>Failed to load</div>
   if (!data) return <div>Loading...</div>
 
   return (
     <div className='text-center py-16 h-full flex-col justify-between items-between'>
       <h1 className='font-semibold text-5xl mb-16'>Emersoft Blog</h1>
 
-      <ModalComponent />
+      <ModalComponent categories={categories} handleCategoryClick={handleCategoryClick} pickedCategory={pickedCategory} />
       <SearchBar handleSearch={handleSearch} />
       {/* {data.map((p: Post) => (
           <>
