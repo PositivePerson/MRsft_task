7116ad554e199dfa1b419d38054f2dcb4e8c573b
Bartek <wonderfulLifeVision@gmail.com>
2022-12-08 16:30:13 +0100

    refactor: change API endpoint props order  AND  add filter functionalliy for: all titles and all categories

--- pages/api/posts/[...props].tsx
+++ pages/api/posts/[...props].tsx
@@ -19,19 +19,18 @@ export default function postHandler(
     console.log("query: ", query);
     console.log("props: ", props);
 
-    const title = props[0]
-    const category = props[1]
-    const startPagination = props[2]
-    const endPagination = props[3]
+    const category = props[0]
+    const startPagination = props[1]
+    const endPagination = props[2]
+    const title = props[3]
 
     console.log("title: ", title);
     console.log("category: ", category);
     console.log("startPagination: ", startPagination);
     console.log("endPagination: ", endPagination);
 
-    // const filtered = []
-    const filteredCategory = blog.posts.filter((p) => p.categories.some((ctgr) => ctgr === Number(category)))
-    const filteredTitle = filteredCategory.filter((p) => p.title.toLowerCase().includes(title.toLowerCase()))
+    const filteredCategory = Number(category) !== 0 ? blog.posts.filter((p) => p.categories.some((ctgr) => ctgr === Number(category))) : blog.posts
+    const filteredTitle = title ? filteredCategory.filter((p) => p.title.toLowerCase().includes(title.toLowerCase())) : filteredCategory
     const filteredPagination = filteredTitle.filter((p, index) => index >= Number(startPagination) && index < Number(endPagination))
 
     return res.status(200).json(filteredPagination)
